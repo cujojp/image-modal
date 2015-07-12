2c59c889b60b55955c080b8c42888695522d2185
cujojp <info@cujo.jp>
2015-07-11 22:04:44 -0700

    initial commit

---
+++ build/tasks/watch.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var handleErrors = require('../utils/handleErrors');
+var notify = require('gulp-notify');
+
+// Start the server
+gulp.task('watch', function() {
+  gulp.watch([
+    'public/javascripts/**/*.js',
+    '!public/javascripts/**/all.js',
+    '!public/javascripts/**/all.min.js'], ['scripts-complexity', 'scripts']);
+  gulp.watch('public/sass/**/*.scss', ['styles']);
+});  
